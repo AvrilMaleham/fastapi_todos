59595245f7b407e3cbb30e36eef46e51309cb330
Avril Maleham <avrilmaleham@outlook.com>
2025-04-05 09:39:26 +1300

    test: finish admin tests

--- test/test_admin.py
+++ test/test_admin.py
@@ -11,3 +11,15 @@ def test_admin_read_all_authenticated(test_todo):
     response = client.get("/admin/todo")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == [{"complete": False, "title": "Learn to code", "description": "Practice daily", "id": 1, "priority": 5, "owner_id": 1  }]
+    
+def test_admin_delete_todo(test_todo):
+    response = client.delete("/admin/todo/1")
+    assert response.status_code == 204 
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model is None
+    
+def test_admin_delete_todo_not_found(test_todo):
+    response = client.delete("/admin/todo/999")
+    assert response.status_code == 404 
+    assert response.json() == {"detail": "Todo not found."}
